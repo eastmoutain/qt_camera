7f5064ad0e941f83b18df578afbe2461d8e703fa
dongshan <east.moutain.yang@gmail.com>
2014-11-03 22:23:00 +0800

    The second edition. Add menu bar, tool bar and status bar.

--- build-camera-Desktop_Qt_5_3_GCC_64bit-Debug/moc_cameral_window.cpp
+++ build-camera-Desktop_Qt_5_3_GCC_64bit-Debug/moc_cameral_window.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../camera/cameral_window.h"
+#include "../../qt_camera/camera/cameral_window.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_cameral_window_t {
-    QByteArrayData data[6];
-    char stringdata[62];
+    QByteArrayData data[7];
+    char stringdata[68];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,10 +34,11 @@ QT_MOC_LITERAL(1, 15, 9),
 QT_MOC_LITERAL(2, 25, 0),
 QT_MOC_LITERAL(3, 26, 12),
 QT_MOC_LITERAL(4, 39, 11),
-QT_MOC_LITERAL(5, 51, 10)
+QT_MOC_LITERAL(5, 51, 10),
+QT_MOC_LITERAL(6, 62, 5)
     },
     "cameral_window\0setCamera\0\0cameraDevice\0"
-    "startCamera\0stopCamera"
+    "startCamera\0stopCamera\0about"
 };
 #undef QT_MOC_LITERAL
 
@@ -47,7 +48,7 @@ static const uint qt_meta_data_cameral_window[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -55,12 +56,14 @@ static const uint qt_meta_data_cameral_window[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x08 /* Private */,
-       4,    0,   32,    2, 0x08 /* Private */,
-       5,    0,   33,    2, 0x08 /* Private */,
+       1,    1,   34,    2, 0x08 /* Private */,
+       4,    0,   37,    2, 0x08 /* Private */,
+       5,    0,   38,    2, 0x08 /* Private */,
+       6,    0,   39,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QByteArray,    3,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -75,6 +78,7 @@ void cameral_window::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         case 0: _t->setCamera((*reinterpret_cast< const QByteArray(*)>(_a[1]))); break;
         case 1: _t->startCamera(); break;
         case 2: _t->stopCamera(); break;
+        case 3: _t->about(); break;
         default: ;
         }
     }
@@ -105,13 +109,13 @@ int cameral_window::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
